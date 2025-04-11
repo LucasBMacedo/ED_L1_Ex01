81c752e76867ed5cf4e733daac5dfc7313889402
lucasbezerrademacedo <lucasbezerrademacedo@192.168.108.133>
2025-04-11 15:38:05 -0300

    Segundo commit.

--- src/controller/SomaController.java
+++ src/controller/SomaController.java
@@ -8,14 +8,13 @@ public class SomaController {
 	
     public int somannatural(int n, int contador, int soma) {
     	if (n < 0) {
-    		return 0; // Retornará 0 caso o usuário insira um número negativo.
+    		return 0; 
     		}
     	if (contador > n) {
-    		return soma; // Ponto de parada - quando o contador passar de n, será retornada a ultima sokma.
+    		return soma; 
     	}
-    		soma = soma + contador; // Incremento da soma com o contador.
-    		contador++; // incremento do contador.
-    		return somannatural(n, contador, soma);
-    		
+    		soma = soma + contador; 
+    		contador++; 
+    		return somannatural(n, contador, soma);   		
     }
 }
